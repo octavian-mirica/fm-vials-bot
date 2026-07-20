d5bebc5aa99b481cef68b43875cdccaef36d59d8
omirica <omirica@lorenz.cc>
2026-07-20 15:45:00 +0300

    New repo

---
+++ index.js
@@ -0,0 +1,20 @@
+const { Client, GatewayIntentBits } = require("discord.js");
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
+
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}`);
+});
+
+client.on("messageCreate", (msg) => {
+  if (msg.author.bot) return;
+  msg.reply("Hello from Vials Bot!");
+});
+
+client.login(process.env.BOT_TOKEN);
